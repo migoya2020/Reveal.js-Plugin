e84a48bb21ef0d8a8f8c05b090c7d51d58715497
David Migoya <dmigoya@gmail.com>
2020-10-27 11:22:40 +0300

    initial commit

---
+++ continousHorizontal.js
@@ -0,0 +1,20 @@
+Reveal.on('slidechanged', event =>
+	{
+	
+		lastSlide = Reveal.getHorizontalSlides().length - 1;
+		horizonSlides = Reveal.getHorizontalSlides();
+		if (lastSlide == event.indexh)
+		{
+            console.log("Last Slide");
+            //change 1st slide to future
+			horizonSlides[0].className = "stack future";
+
+		}
+		else if (event.indexh == 0)
+		{  	
+            console.log("This is 1st slide");
+            //change  last slide class
+			horizonSlides[lastSlide].className = "stack past";
+			
+		}
+	});
